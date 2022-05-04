18e1575c4462a7180b982baeb10d3aae2bc936b2
csachin42 <csachin42@gmail.com>
2022-05-04 20:32:11 +0530

    Welcome msg

---
+++ Display.java
@@ -0,0 +1,8 @@
+package Fibonacci1;
+
+public class Display {
+    public void display(){
+        System.out.println("Hii welcome to Fibonacci program");
+
+    }
+}
---
+++ fibonaci.java
@@ -0,0 +1,25 @@
+package Fibonacci1;
+import java.util.Scanner;
+
+class Fibonaci {
+    int input,temp=0,temp1=1,sum;
+    void input(){                // Taking input from user//
+        System.out.println("Enter the number of Element");  // user will get
+        Scanner sc=new Scanner(System.in);
+        input=sc.nextInt();
+    }
+    void output(){                        // Output method for calculation//
+        int j=0;
+        for (j=0;j<=input;++j){
+            System.out.println(sum);    // Output will be displayed in terminal//
+            sum=temp+temp1; // Adding initial two values
+            temp=temp1;    // swaping temp1 value to temp
+            temp1=sum;    // swaping sum to temp 1
+        }
+    }
+    public static void main(String[] args) {
+        Fibonaci f=new Fibonaci();
+        f.input();
+        f.output();
+    }
+}
